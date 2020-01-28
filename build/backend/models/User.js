cba774206e1430db4ce3101d1855bcb278e1c1a0
Aaron Jackson <aaronjackson@aarons-mbp.attlocal.net>
2020-01-27 23:28:16 -0800

    change

--- build/backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-let User = new Schema({
-    username: { type: String },
-    passphrase: { type: String },
-});
-
-module.exports = User;
